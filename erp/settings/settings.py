a74843fdb54997958556d021108fdbc4fb0aa780
Dorin <racovetdorin@gmail.com>
2023-01-23 17:53:54 +0200

    Change database settings for github workflow

--- erp/settings/settings.py
+++ erp/settings/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
+import os
 
 from decouple import config
 from pathlib import Path
@@ -64,6 +65,18 @@ DATABASES = {
         'PORT': config('PSQL_DB_PORT', '5432'),
     },
 }
+
+if os.environ.get('GITHUB_WORKFLOW'):
+    DATABASES = {
+        'default': {
+           'ENGINE': 'django.db.backends.postgresql',
+           'NAME': 'github_actions',
+           'USER': 'postgres',
+           'PASSWORD': 'postgres',
+           'HOST': '127.0.0.1',
+           'PORT': '5432',
+        }
+    }
 
 TEMPLATES = [
     {
