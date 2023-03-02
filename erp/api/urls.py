2911bb2986c1dc8d56f6520e3b05c6f67019fae3
Dorin <racovetdorin@gmail.com>
2023-03-02 11:45:44 +0200

    fix urls

--- erp/api/urls.py
+++ erp/api/urls.py
@@ -6,7 +6,6 @@ from rest_framework_simplejwt.views import TokenRefreshView
 
 
 urlpatterns = [
-    path('', views.getroutes),
     path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
